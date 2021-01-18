fc03c21e74d0f9422155e362e735d19e2b6aa7c8
Andrey Zhizhov <gitzhizhov@github.com>
2021-01-18 23:36:41 +0500

    Примерный внешний вид

---
+++ Model/EmailSendServiceClass.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mailer.Model
+{
+    class EmailSendServiceClass
+    {
+        #region vars
+        private string strLogin;         // email, c которого будет рассылаться почта
+        private string strPassword;  // пароль к email, с которого будет рассылаться почта
+        private string strSmtp = "smtp.yandex.ru"; // smtp-server
+        private int iSmtpPort = 25;                // порт для smtp-server
+        #endregion
+        public EmailSendServiceClass(string sLogin, string sPassword)
+        {
+            strLogin = sLogin;
+            strPassword = sPassword;
+        }
+
+        private void SendMail(string mail, string name) // Отправка email конкретному адресату
+        {
+            using (MailMessage mm = new MailMessage(strLogin, mail))
+            {
+                mm.Subject = "none";//strSubject;
+                mm.Body = "Hello world!";
+                mm.IsBodyHtml = false;
+                SmtpClient sc = new SmtpClient(strSmtp, iSmtpPort);
+                sc.EnableSsl = true;
+                sc.DeliveryMethod = SmtpDeliveryMethod.Network;
+                sc.UseDefaultCredentials = false;
+                sc.Credentials = new NetworkCredential(strLogin, strPassword);
+                try
+                {
+                    sc.Send(mm);
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine("Невозможно отправить письмо " + ex.ToString());
+                }
+            }
+        }//private void SendMail(string mail, string name)
+
+    }  //private void SendMail(string mail, string name)
+
+    //Альтернативный класс для отправки
+    static class ServiceData
+    {
+        //static public List<string> SmptClients = new List<string>()
+        //{ "smtp.yandex.ru", "smtp.gmail.com","smtp.mail.ru" };
+        //    static public List<int> Ports = new List<int>() { 25, 58, 25 };
+        static public List<SmtpClient> SmtpClients { get; } = new List<SmtpClient>() { new SmtpClient("smtp.yandex.ru", 25), new SmtpClient("smtp.gmail.com", 58), new SmtpClient("smtp.mail.ru", 25) };
+
+    }
+
+
+    class EMailSendServiceClass
+    {
+
+
+
+
+
+        static public event Action<string> Action;//Любое событие может вызываться только внутри класса в котором оно определено
+        public void Send(MailMessage message, string password, int port)
+        {
+            try
+            {
+                //var fromAddress = new MailAddress("geekbrains2021@gmail.com", "Tester");
+                //var toAddress = new MailAddress("geekbrains2021@gmail.com", "Tester");
+                //Преобазование из Windows-1251 в UTF-8
+                string subject = message.Subject;// tbSubject.Text;
+                //string fio = "from me";
+                //rtbBody.SelectAll();
+                password = System.IO.File.ReadAllText("C:\\temp\\1.txt");//  tbPassword.Password;
+                string body = message.Body;// tbBody.Text; //rtbBody.Selection.Text; // dataTasks.Value + "\r\n" + dataResult.Value;// win1251.GetString(win1251Bytes3)+"\r\n"+ win1251.GetString(win1251Bytes2);                
+                var smtp = new SmtpClient()
+                {
+                    Host = "smtp.gmail.com",
+                    Port = port,
+                    EnableSsl = true,
+                    DeliveryMethod = SmtpDeliveryMethod.Network,
+                    UseDefaultCredentials = false,
+                    Credentials = new NetworkCredential("geekbrains2021@gmail.com", password)
+                };
+                Action?.Invoke("Message is sending");
+                smtp.Send(message);
+                //MessageBox.Show("Message has sent");
+                //Debug.WriteLine("Message has sent");
+                Action?.Invoke("Message has sent");
+            }
+            catch (Exception ex)
+            {
+                //using System.Diagnostic
+                Debug.WriteLine(ex);
+                //Console.WriteLine(ex);
+                Action?.Invoke(ex.Message);
+            }
+
+
+        }
+    }
+}
---
+++ View/MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Mailer.Model;
+
+namespace Mailer
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void EMailSendServiceClass_Action(string message)
+        {
+            tbLog.Text += DateTime.Now + ":" + message + "\r\n";
+            Console.WriteLine(DateTime.Now + ":" + message + "\r\n");
+        }
+
+        private void btnSendAtOnce_Click(object sender, RoutedEventArgs e)
+        {
+            //string strLogin = cbSenderSelect.Text;
+            //string strPassword = cbSenderSelect.SelectedValue.ToString();
+            string strLogin = tbUserName.Text;
+            string strPassword = tbPassword.Password;
+            Console.WriteLine("Данные взяли из полей настроек");
+            if (string.IsNullOrEmpty(strLogin))
+            {
+                MessageBox.Show("Выберите отправителя");
+                return;
+            }
+            if (string.IsNullOrEmpty(strPassword))
+            {
+                MessageBox.Show("Укажите пароль отправителя");
+                return;
+            }
+
+            //            EMailInfo eMailInfo = new EMailInfo();
+            MailMessage mm = new MailMessage(cbFrom.Text, cbTo.Text, tbSubject.Text, tbBody.Text);
+
+            EMailSendServiceClass emailSender = new EMailSendServiceClass();
+            emailSender.Send(mm, System.IO.File.ReadAllText("C:\\Temp\\1.txt"), 587);
+        }
+
+        private void btnSend2_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnPrev_Click(object sender, RoutedEventArgs e)
+        {
+            if (tabControl.SelectedIndex == 0)
+                tabControl.SelectedIndex = tabControl.Items.Count - 1;
+            else
+                tabControl.SelectedIndex = tabControl.SelectedIndex - 1;
+        }
+
+        private void btnNext_Click(object sender, RoutedEventArgs e)
+        {
+            //tabControl.SelectedIndex = tabControl.SelectedIndex + 1;
+            tabControl.SelectedIndex = (tabControl.SelectedIndex + 1) % tabControl.Items.Count;
+        }
+    }
+}
